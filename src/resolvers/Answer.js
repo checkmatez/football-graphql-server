01bf5ad75f023bf364f38bcb6838c951ff0773ea
Max Davidov <davidovmk@yandex.ru>
2018-02-26 15:21:42 +0300

    add dispute field to Answer, state to Match

--- src/resolvers/Answer.js
+++ src/resolvers/Answer.js
@@ -3,14 +3,15 @@ const fetch = require('node-fetch')
 const baseUrl = 'https://api.football.dev.highglossy.com'
 
 module.exports = {
+  createdAt: ({ respondedAt }) => respondedAt,
   state: ({ state }) => state.toUpperCase(),
   creator: async ({ userId }, args, { accessToken }) => {
     try {
       const response = await fetch(`${baseUrl}/ratings?id=${userId}`, {
         headers: {
           Authorization: accessToken,
-          'Content-Type': 'application/json',
-        },
+          'Content-Type': 'application/json'
+        }
       })
       const result = await response.json()
       if (result.error) {
@@ -21,21 +22,19 @@ module.exports = {
       throw error
     }
   },
-  // dispute: async ({ disputeId }) => {
-  //   try {
-  //     const response = await fetch(`${baseUrl}/ratings?id=${userId}`, {
-  //       headers: {
-  //         Authorization: accessToken,
-  //         'Content-Type': 'application/json',
-  //       },
-  //     })
-  //     const result = await response.json()
-  //     if (result.error) {
-  //       throw new Error(result.error.message || 'Server error')
-  //     }
-  //     return result.data.user
-  //   } catch (error) {
-  //     throw error
-  //   }
-  // },
+  dispute: async (parent, { disputeId }, { accessToken }) => {
+    try {
+      const response = await fetch(`${baseUrl}/disputes/?id=${disputeId}`, {
+        headers: { Authorization: accessToken }
+      })
+      const result = await response.json()
+      if (result.error) {
+        throw new Error(result.error.message || 'Server error')
+      }
+      console.log('result', result)
+      return result.data
+    } catch (error) {
+      throw error
+    }
+  }
 }
