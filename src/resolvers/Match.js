b54d6667bd56e067b08f846567b5e2bcfeb8d76f
Max Davidov <davidovmk@yandex.ru>
2018-02-27 10:54:30 +0300

    fix match state

--- src/resolvers/Match.js
+++ src/resolvers/Match.js
@@ -2,4 +2,5 @@ module.exports = {
   id: ({ championatId }) => championatId,
   startedAt: ({ startedAt }) =>
     `${startedAt.substr(0, 10)}T${startedAt.substr(11)}Z`,
+  state: ({ state }) => state.toUpperCase()
 }
