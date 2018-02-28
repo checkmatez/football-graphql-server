80d1ada7a328fe59d5e026300a407bd704ec336b
Max Davidov <davidovmk@yandex.ru>
2018-02-28 15:30:46 +0300

    fix match on Dispute

--- src/resolvers/Dispute.js
+++ src/resolvers/Dispute.js
@@ -1,3 +1,9 @@
+const fetch = require('node-fetch')
+const querystring = require('querystring')
+const winston = require('winston')
+
+const { BASE_URL } = require('../constants')
+
 const Dispute = {
   createdAt: ({ createdAt }) =>
     `${createdAt.substr(0, 10)}T${createdAt.substr(11)}Z`,
@@ -9,6 +15,29 @@ const Dispute = {
     confirmation ? confirmation.toUpperCase() : null,
   creator: ({ user }) => user,
   answers: ({ responses }) => responses || [],
+  match: async ({ match, matchId }, args, { accessToken }) => {
+    if (match) {
+      return match
+    }
+    const params = querystring.stringify({
+      championatId: matchId
+    })
+    const url = `${BASE_URL}/matches?${params}`
+    winston.info('url', url)
+    try {
+      const response = await fetch(url, {
+        headers: { Authorization: accessToken }
+      })
+      const result = await response.json()
+      if (result.error) {
+        throw new Error(result.error.message || 'Server error')
+      }
+      winston.info('result', JSON.stringify(result, null, 2))
+      return result.data.find(m => m.championatId === matchId)
+    } catch (error) {
+      throw error
+    }
+  }
 }
 
 module.exports = Dispute
